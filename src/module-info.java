59b54211e5be124bcf7b5e5b96056e7deeb29b99
H3Xobit <121175445+H3Xobit@users.noreply.github.com>
2023-01-05 17:13:07 +0530

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @author Rishi
+ *
+ */
+module scientific_calculator {
+	requires java.desktop;
+	requires java.logging;
+	requires java.base;
+}
---
+++ src/scientific_calculator/CalculatorUI.java
@@ -0,0 +1,10 @@
+package scientific_calculator;
+
+public class CalculatorUI {
+
+	public void setVisible(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
